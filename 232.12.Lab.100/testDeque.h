13a89fb5fbe72f5acdfc864c5c829902d56e11ee
E Harrison <har16020@byui.edu>
2022-12-03 17:03:11 -0700

    removed // test flagged comments

--- 232.12.Lab.100/testDeque.h
+++ 232.12.Lab.100/testDeque.h
@@ -35,7 +35,7 @@ public:
       // Construct
       test_construct_default();
       test_constructCopy_empty();
-      test_constructCopy_standard();
+      test_constructCopy_standard(); // test flagged
 
       // Assign
       test_assign_emptyToEmpty();
@@ -52,7 +52,7 @@ public:
       test_iterator_increment_standardMiddle();
       test_iterator_dereferenceRead_standard();
       test_iterator_dereferenceRead_wrap();
-      test_iterator_dereferenceRead_wrapNegative(); // test flagged
+      test_iterator_dereferenceRead_wrapNegative();
       test_iterator_dereferenceWrite_standard();
       test_iterator_difference_standard();
       test_iterator_addonto_standard();
@@ -60,15 +60,15 @@ public:
       // Access
       test_frontRead_standard();
       test_frontRead_wrap();
-      test_frontRead_wrapNegative(); // test flagged
+      test_frontRead_wrapNegative();
       test_frontWrite_standard();
       test_backRead_standard();
       test_backRead_wrap();
-      test_backRead_wrapNegative(); // test flagged
+      test_backRead_wrapNegative();
       test_backWrite_standard();
       test_subscriptRead_standard();
       test_subscriptRead_wrap();
-      test_subscriptRead_wrapNegative(); // test flagged
+      test_subscriptRead_wrapNegative();
       test_subscriptWrite_standard();
 
       // Insert
@@ -76,10 +76,10 @@ public:
       test_pushback_room();
       test_pushback_grow();
       test_pushback_growWrap();
-      test_pushfront_empty(); // test flagged
-      test_pushfront_room(); // test flagged
-      test_pushfront_grow(); // test flagged
-      test_pushfront_growWrap(); // test flagged
+      test_pushfront_empty();
+      test_pushfront_room();
+      test_pushfront_grow();
+      test_pushfront_growWrap();
 
       // Remove
       test_clear_empty();
